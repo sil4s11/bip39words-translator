823558853f868198da8d730fbd043d09c3972feb
Fran <kikofran86@gmail.com>
2023-01-17 21:45:32 +0100

    Fix alias stream

--- vite.config.ts
+++ vite.config.ts
@@ -68,7 +68,7 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
       buffer: "rollup-plugin-node-polyfills/polyfills/buffer-es6",
       process: "rollup-plugin-node-polyfills/polyfills/process-es6",
-      stream: "rollup-plugin-node-polyfills/polyfills/stream-es6",
+      stream: "rollup-plugin-node-polyfills/polyfills/stream",
     },
   },
 });
