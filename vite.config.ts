3d8ebb52e71d8baecca2b387b4d42f6fad14d14a
Fran <kikofran86@gmail.com>
2023-01-21 18:03:48 +0100

    Clean alias vite config

--- vite.config.ts
+++ vite.config.ts
@@ -75,16 +75,6 @@ export default defineConfig({
       crypto: "rollup-plugin-node-polyfills/polyfills/crypto-browserify",
       stream: "rollup-plugin-node-polyfills/polyfills/stream",
       util: "rollup-plugin-node-polyfills/polyfills/util",
-      _stream_duplex:
-        "rollup-plugin-node-polyfills/polyfills/readable-stream/duplex",
-      _stream_passthrough:
-        "rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough",
-      _stream_readable:
-        "rollup-plugin-node-polyfills/polyfills/readable-stream/readable",
-      _stream_writable:
-        "rollup-plugin-node-polyfills/polyfills/readable-stream/writable",
-      _stream_transform:
-        "rollup-plugin-node-polyfills/polyfills/readable-stream/transform",
     },
   },
   build: {
